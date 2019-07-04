47c38ac214e0fbc27cf185301b86b7f562f93f90
Elise <elisegamassa@gmail.com>
2019-07-04 13:16:21 +0200

    2nd update

--- bin/Recipes.js
+++ bin/Recipes.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose");
-const Movie = require("../models/Recipe");
+const Recipe = require("../models/Recipe");
 
 const dbName = "base-recipes";
-mongoose.connect(`mongodb://localhost/${dbName}`);
+mongoose.connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true });
 
 const recipes = [
   {
     Name: "Cookie",
     Region: "America",
     Description: "Delicious cookies",
-    Ingrédients:
-      "1 cup salted butter softened 1 cup white (granulated) sugar 1 cup light brown sugar packed 2 tsp pure vanilla extract 2 large eggs 3 cups all-purpose flour 1 tsp baking soda ½ tsp baking powd"
+    Ingrédients: [
+      "1 cup salted butter softened",
+      "1 cup white (granulated) sugar",
+      "1 cup light brown sugar packed",
+      "2 tsp pure vanilla extract",
+      "2 large eggs",
+      "3 cups all-purpose flour",
+      "1 tsp baking soda",
+      "½ tsp baking powder"
+    ],
+    image:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjhou-cjpvjAhWSMBQKHYTIDqAQjRx6BAgBEAU&url=https%3A%2F%2Fcuisine.journaldesfemmes.fr%2Frecette%2F310548-american-cookies&psig=AOvVaw1XbF1ux9OAXkq1Lq1BQ-Hv&ust=1562324442016491"
   },
   {
     Name: "Bibimbap",
     Region: "Asia/South-Korea",
-    Description: "",
+    Description: "Korean rice and legumes",
     Ingrédients: [
       "Riz",
       "2 carottes",
@@ -25,7 +35,9 @@ const recipes = [
       "Champignons shiitakés ou de Paris",
       "2 ou 3 gousses d'ail",
       "1 morceau de gingembre râpé"
-    ]
+    ],
+    image:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjNipa0jpvjAhXB8-AKHa2ODLkQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBibimbap&psig=AOvVaw1Hhwutk94-LCbt-qVmeNa8&ust=1562324489134668"
   }
 ];
 
