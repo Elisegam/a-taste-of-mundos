266db63c847c2ee92f657a233eabd81709b1746a
Maxtanie Petit Dol <maxtaniepetitdol@outlook.fr>
2019-07-04 18:33:05 +0200

    last modif maxtanie

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,38 @@ const mongoose = require("mongoose");
 
 const Recipe = require("../models/Recipe.js");
 
-/* GET home page */
-router.get("/", (req, res, next) => {
-  var q = req.query.q;
-  Recipe.find(
-    {
-      $text: { $search: q }
-    },
-    {
-      _id: 0,
-      __v: 0
-    },
-    function(err, data) {
-      res.json(data);
-    }
-  )
-    .then(recipes => console.log(recipes))
-    .catch(e => console.log(e));
-});
-
-// Recipe.createIndex({
-//   Name: "text",
-//   Region: "text",
-//   image: "text"
+// /* GET home page */
+// router.get("/", (req, res, next) => {
+//   var q = req.query.q;
+//   Recipe.find(
+//     {
+//       $text: { $search: q }
+//     },
+//     {
+//       _id: 0,
+//       __v: 0
+//     },
+//     function(err, data) {
+//       res.json(data);
+//     }
+//   )
+//     .then(recipes => console.log(recipes))
+//     .catch(e => console.log(e));
 // });
+
+// // Recipe.createIndex({
+// //   Name: "text",
+// //   Region: "text",
+// //   image: "text"
+// // });
 
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/result", (req, res, next) => {
+  res.render("result");
+});
 /* GET see-moire page */
 router.get("/see-more", (req, res, next) => {
   res.render("see-more");
