9cd7c11a014ebab1098290b2704412a691f51353
Elise <elisegamassa@gmail.com>
2019-07-05 16:14:38 +0200

    fea

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose
   .catch(err => {
     console.error("Error connecting to mongo", err);
   });
-require("./bin/worldApi");
+
 const app_name = require("./package.json").name;
 const debug = require("debug")(
   `${app_name}:${path.basename(__filename).split(".")[0]}`
