95aba6b110ae64242912172d6356319704b361ab
Maxtanie Petit Dol <maxtaniepetitdol@outlook.fr>
2019-07-05 10:17:13 +0200

    last modification

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = new express.Router();
 const mongoose = require("mongoose");
 
-const Recipe = require("../models/Recipe");
+const Recipe = require("../models/Recipe.js");
 
-<<<<<<< HEAD
 // /* GET home page */
 // router.get("/", (req, res, next) => {
 //   var q = req.query.q;
@@ -29,43 +28,15 @@ const Recipe = require("../models/Recipe");
 // //   Region: "text",
 // //   image: "text"
 // // });
-/* GET home page */
+
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.post("/", (req, res, next) => {
-  // console.log(req.body);
-  const query = req.body.query;
-  const regex = new RegExp(query, "i");
-  Recipe.find({ Name: regex })
-    .then(recipes => {
-      res.send(recipes);
-    })
-    .catch(e => console.log(e));
+router.get("/result", (req, res, next) => {
+  res.render("result");
 });
-
-// router.get("/search/resto", (req, res) => {
-//   // console.log(req.query);
-//   const regex = new RegExp(req.query.search, "i");
-//   Restaurant.find({ name: regex })
-//     .then(restos => {
-//       console.log(restos);
-//       res.json(restos);
-//     })
-//     .catch(err => console.error(err));
-// });
-
-<<<<<<< HEAD
-/* GET home page */
-=======
->>>>>>> 9bd54dafb27cea82b67467deaddf33e5e2773183
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
-
 /* GET see-moire page */
->>>>>>> 2f631c4d36ba510a9d2cbdbb4895ed1bfb306b6d
 router.get("/see-more", (req, res, next) => {
   res.render("see-more");
 });
@@ -75,7 +46,3 @@ router.get("/favourite-foods", (req, res, next) => {
   res.render("favourite-foods");
 });
 module.exports = router;
-
-router.get("/result", (req, res, next) => {
-  res.render("result");
-});
